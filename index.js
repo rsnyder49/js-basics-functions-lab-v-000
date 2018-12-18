3ff4b39d7a1860ab4d8638d9af5bb5462ab8d252
Robert Snyder <r.snyder49@gmail.com>
2018-12-18 23:01:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  let distance = '';
+  if (blocks > 42) {
+    distance = blocks - 42;
+  }
+  else {
+    distance = 42 - blocks;
+  }
+  return distance;
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(start, end ) {
+  let totalDistance = '';
+  if (start > end) {
+    totalDistance = (start - end) * 264;
+  }
+  else {
+    totalDistance = (end - start) * 264;
+  }
+  return totalDistance;
+}
+
+function calculatesFarePrice(start, end) {
+  let totalPrice = 0;
+  let distance = distanceTravelledInFeet(start, end)
+  if (distance < 400) {
+    return totalPrice;
+  }
+  else if (distance > 400 && distance < 2000) {
+     totalPrice =  .02 * (distance - 400);
+   } 
+  else if (distance > 2000 && distance < 2500) {
+     totalPrice = 25;
+   } 
+  else {
+    return "cannot travel that far";
+   }
+   return totalPrice;
+}
+
+
+
